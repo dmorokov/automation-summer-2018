301e1a119378cd4d396d5f97ae63433e1da97e52
Danila_Morokov <dmorokov@yandex.ru>
2018-05-30 20:13:52 +0300

    lesson 6

---
+++ src/main/java/pageObjects/HomePageCucumberSelenide.java
@@ -0,0 +1,61 @@
+package pageObjects;
+
+import com.codeborne.selenide.SelenideElement;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by Danila_Morokov on 5/23/2018.
+ */
+public class HomePageCucumberSelenide {
+
+    @FindBy(css = ".profile-photo")
+    private SelenideElement userIcon;
+
+    @FindBy(css = "#Name")
+    private SelenideElement loginInput;
+
+    @FindBy(css = "#Password")
+    private SelenideElement passwordInput;
+
+    @FindBy(css = ".form-horizontal button[type = 'submit']")
+    private SelenideElement submitButton;
+
+    @FindBy(css = ".profile-photo span")
+    private SelenideElement userName;
+
+    @Step
+    @Given("I'm on the Home Page")
+    public void openHomePage() {
+        open("https://epam.github.io/JDI/index.html");
+    }
+
+    @Step("Login")
+    @When("I login as user (.+) with password (.+)")
+    public void login(String login, String password) {
+        userIcon.click();
+        loginInput.sendKeys(login);
+        passwordInput.sendKeys(password);
+        submitButton.click();
+    }
+
+    @Step
+    @Then("The browser title is Home Page")
+    public void checkHomePageTitle() {
+        assertEquals(getWebDriver().getTitle(), "Home Page");
+    }
+
+    @Step
+    @Then("The user icon is displayed on the header")
+    public void checkUserName() {
+        assertTrue(userName.isDisplayed());
+    }
+}
